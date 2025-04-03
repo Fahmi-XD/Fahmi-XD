39c714c93c83d4c54bfe3d7f117fd2142e3d9c49
Fahmi <121919479+Fahmi-XD@users.noreply.github.com>
2025-04-03 08:36:03 +0700

    Add files via upload

---
+++ pix/model.js
@@ -0,0 +1,4 @@
+const model = [
+  "meinamix_meinaV11.safetensors", // Ini kayaknya model biasa
+  "AbsoluteReality_v1.8.1.safetensors" // Pake ini kalo mau realisktik dan loranya pake yang real juga
+]
